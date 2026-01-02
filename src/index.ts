2cb8c4eb86d60d8a728095012f463f8e8115bb2f
Yathaartha <maharjanyathaartha017@gmail.com>
2026-01-02 13:46:26 -0500

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import type { Request, Response } from "express";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Stay Tracker API is running!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
